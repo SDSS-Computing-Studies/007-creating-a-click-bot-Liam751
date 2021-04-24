febaca60c4f5914e1ba2f6c461ce6ddcfef6e169
Liam751 <55156691+Liam751@users.noreply.github.com>
2021-04-23 21:35:28 -0700

    Update boy.py

--- boy.py
+++ boy.py
@@ -1,3 +1,24 @@
 import pyautogui
+import time
 
-pyautogui.mouseInfo()
+a = 1
+
+def main():
+    while a <= 10: 
+        curTime = time.time()
+        first()
+        second(a)
+        print("aaa")
+
+def first():
+    pyautogui.click(611, 397)
+    pyautogui.click(519, 416)
+
+def second(a):
+    pyautogui.click(846, 333)
+
+
+main()
+
+
+
